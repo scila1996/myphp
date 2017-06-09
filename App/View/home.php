71d9b83d6a11598d0189a39ed549fcc36d91a655
scila1996 <iucuaong_1996@yahoo.com.vn>
2017-06-09 17:04:58 +0700

    Signed-off-by: scila1996 <iucuaong_1996@yahoo.com.vn>

--- App/View/home.php
+++ App/View/home.php
@@ -2,11 +2,11 @@
 <html>
 	<head>
 		<title> Test PHP </title>
-		<link rel="stylesheet" href="/extensions/bootstrap-3.3.7-dist/css/bootstrap.min.css" />
-		<link rel="stylesheet" href="/extensions/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" />
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-		<script src="/extensions/jquery-1.12.4.min.js"></script>
-		<script src="/extensions/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+		<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<div class="container">
