98f9608c624643b9fe53a5d28d88f81cd1362c86
scila1996 <iucuaong_1996@yahoo.com.vn>
2017-06-12 09:00:57 +0700

    Signed-off-by: scila1996 <iucuaong_1996@yahoo.com.vn>

--- App/Config/Route.php
+++ App/Config/Route.php
@@ -5,3 +5,5 @@ use System\Config\Route;
 Route::get("/", "Home::index");
 Route::get("/get/{id:\d+}", "Home::index");
 Route::post("/get/{id:\d+}", "Home::test");
+
+Route::get("/database", "Home::database");
--- App/Controller/Home.php
+++ App/Controller/Home.php
@@ -4,6 +4,12 @@ namespace App\Controller;
 
 use System\Libraries\View;
 
+require 'database-5.4/vendor/autoload.php';
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Container\Container;
+
 class Home extends \System\Core\Controller
 {
 
@@ -19,6 +25,16 @@ class Home extends \System\Core\Controller
 		View::add("test.php", array(
 			"content" => var_export($this->request->getParsedBody(), TRUE)
 		));
+	}
+
+	public function database()
+	{
+		$pdo = new \PDO("mysql:host=localhost;dbname=data;charset=utf8", "root", "");
+		$connect = new \Illuminate\Database\SqlServerConnection($pdo, "data");
+		$query = $connect->table("abc")->select();
+		$query->where("id", 5)->join("def", "id", "id");
+		echo $query->grammar->compileUpdate($query, array("id" => 5));
+		var_dump($query->getBindings());
 	}
 
 }
