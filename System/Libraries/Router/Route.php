73dcdf5e8c077a3d0af43e7613ccc0ce14d80018
scila1996 <iucuaong_1996@yahoo.com.vn>
2017-06-14 16:07:12 +0700

    Signed-off-by: scila1996 <iucuaong_1996@yahoo.com.vn>

--- System/Libraries/Router/Route.php
+++ System/Libraries/Router/Route.php
@@ -26,49 +26,9 @@ class Route
 
 	public static $routes = array();
 
-	private static function add($httpMethod, $pattern, $handler)
+	public static function __callStatic($name, $arguments)
 	{
-		array_push(self::$routes, func_get_args());
-	}
-
-	public static function get($pattern, $handler)
-	{
-		self::add("GET", $pattern, $handler);
-	}
-
-	public static function post($pattern, $handler)
-	{
-		self::add("POST", $pattern, $handler);
-	}
-
-	public static function put($pattern, $handler)
-	{
-		self::add("PUT", $pattern, $handler);
-	}
-
-	public static function delete($pattern, $handler)
-	{
-		self::add("DELETE", $pattern, $handler);
-	}
-
-	public static function head($pattern, $handler)
-	{
-		self::add("HEAD", $pattern, $handler);
-	}
-
-	public static function patch($pattern, $handler)
-	{
-		self::add("PATCH", $pattern, $handler);
-	}
-
-	public static function options($pattern, $handler)
-	{
-		self::add("OPTIONS", $pattern, $handler);
-	}
-
-	public static function any($pattern, $handler)
-	{
-		self::add("ANY", $pattern, $handler);
+		array_push(self::$routes, array_merge(array($name), $arguments));
 	}
 
 }
